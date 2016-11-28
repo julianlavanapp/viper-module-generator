3c2b62ee9c9bd8f7e598f53b445292d99f0b90b5
Julián Alonso Carballo <julian@lavanapp.com>
2016-11-28 15:18:37 +0100

    Default path added, i always use ./ then wasn`t write it all time

--- lib/vipergen/viperthor.rb
+++ lib/vipergen/viperthor.rb
@@ -6,7 +6,7 @@ module Vipergen
 	desc "generate", "Generate a VIPER module"
 	option :language, :required => false, :default => 'swift', :type => :string, :desc => "The language of the generated module (swift, objc)"
 	option :template, :required => false, :default => 'lavanapp', :type => :string , :desc => "Template for the generation"
-	option :path, :required => true, :type => :string , :desc => "Path where the output module is going to be saved"
+	option :path, :required => false, :default => './', :type => :string , :desc => "Path where the output module is going to be saved"
 	def generate(name)
 	   Vipergen::Generator.generate_viper(options[:template], options[:language], name, options[:path])
 	end
